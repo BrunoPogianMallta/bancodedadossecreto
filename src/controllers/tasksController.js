bf2202acabdd2faf4153d249ed1f7a61925f4f2a
BrunoPogianMallta <brunopmalltaads@gmail.com>
2022-02-28 21:48:48 -0300

    fixes

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const Task = require('../models/tasks');
+const ULID = require('ulid')
 
 
 const router = express.Router();
 
 //cadastrar task
 router.post('/register',async(req,res)=>{
-    const task = req.body;
+    const {title,description}= req.body;
+    const id = ULID.ulid();
     try {
-        const registerTask = await Task.create(task)
+        const registerTask = await Task.create({id,title,description});
         return res.status(200).json(registerTask)  
     } catch (error) {
       return res.status(200).json(error);  
